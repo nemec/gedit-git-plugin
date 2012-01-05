ec9e7bb5f7fd7a2d7081e776a8174e517d628748
Nemec <djnemec@gmail.com>
2012-01-04 23:37:11 -0500

    Added gitignore and empty README.

--- gitedit.py
+++ gitedit.py
@@ -1,5 +1,6 @@
 import os.path
-from gi.repository import GObject, Gedit, Gtk
+from threading import Lock
+from gi.repository import GObject, Gedit, Gtk, Gio
 try:
   from git import Repo
   from git.exc import InvalidGitRepositoryError
@@ -16,7 +17,7 @@ class GitEdit(GObject.Object, Gedit.WindowActivatable):
     self.widget = None
 
   def build_widget(self):
-    self.widget = GitWidget()
+    self.widget = GitWidget(self.window)
     panel = self.window.get_bottom_panel()
     icon = Gtk.Image.new_from_stock(Gtk.STOCK_SAVE, Gtk.IconSize.MENU)
     panel.add_item(self.widget, "Git Integration", "Git Integration", icon)
@@ -49,12 +50,12 @@ class GitEdit(GObject.Object, Gedit.WindowActivatable):
 
 class GitWidget(Gtk.Notebook):
 
-  def __init__(self):
+  def __init__(self, window):
     Gtk.Notebook.__init__(self)
     self.pages = {}
-    self.pages["init"] = self.append_page(InitBox(), None)
-    self.pages["repo"] = self.append_page(RepoBox(), None)
-    self.pages["error"] = self.append_page(ErrorBox(), None)
+    self.pages["init"] = self.append_page(InitBox(window), None)
+    self.pages["repo"] = self.append_page(RepoBox(window), None)
+    self.pages["error"] = self.append_page(ErrorBox(window), None)
     self.set_show_tabs(False)
     
     self.set_page("repo")
@@ -84,7 +85,7 @@ class GitWidget(Gtk.Notebook):
 
 class ErrorBox(Gtk.Label):
   """ErrorBox is the view displayed when an error occurs with the plugin."""
-  def __init__(self):
+  def __init__(self, window):
     Gtk.Label.__init__(self)
     self.show_all()
 
@@ -99,7 +100,7 @@ class InitBox(Gtk.Box):
   controlled. It provides buttons to initialize a new git repository.
 
   """
-  def __init__(self):
+  def __init__(self, window):
     Gtk.Box.__init__(self)
     self.label = Gtk.Label("Init")
     self.pack_start(self.label, True, True, 0)
@@ -110,9 +111,59 @@ class InitBox(Gtk.Box):
     pass
 
 class RepoBox(Gtk.Box):
-  def __init__(self):
+  def __init__(self, window):
     Gtk.Box.__init__(self)
-    self.last_repo = None
+    self.window = window
+    self.current_repo = None
+
+    self.commit_lock = Lock()
+
+    dobuttons = Gtk.Box()
+    dobuttons.set_orientation(Gtk.Orientation.VERTICAL)
+    self.refresh = Gtk.Button("Refresh")
+    self.refresh.connect("clicked",
+      lambda x: self.update(repo=self.current_repo))
+    dobuttons.pack_start(self.refresh, False, False, 1)
+    commit = Gtk.Button("Commit")
+    commit.connect("clicked", self.commit_files)
+    dobuttons.pack_start(commit, False, False, 1)
+    self.pack_start(dobuttons, False, False, 1)
+    
+    # Section for Unstaged Files
+    unstaged_box = Gtk.Box()
+    unstaged_box.set_orientation(Gtk.Orientation.VERTICAL)
+    scroll = Gtk.ScrolledWindow()
+    self.unstaged_view = Gtk.TreeView()
+    scroll.add(self.unstaged_view)
+    self.unstaged_view.get_selection().set_mode(Gtk.SelectionMode.NONE)
+    unstaged_toggle = Gtk.CellRendererToggle()
+    def on_unstaged_toggle(widget, path):
+      store = self.unstaged_view.get_model()
+      store[path][0] = not store[path][0]
+    unstaged_toggle.connect("toggled", on_unstaged_toggle)
+    unstaged_column_toggle = Gtk.TreeViewColumn("", unstaged_toggle, active=0)
+    self.unstaged_view.append_column(unstaged_column_toggle)
+    unstaged_type = Gtk.CellRendererText()
+    unstaged_column_type = Gtk.TreeViewColumn("",
+      unstaged_type, text=1)
+    self.unstaged_view.append_column(unstaged_column_type)
+    unstaged_text = Gtk.CellRendererText()
+    unstaged_column_text = Gtk.TreeViewColumn("Unstaged Files",
+      unstaged_text, text=2)
+    self.unstaged_view.append_column(unstaged_column_text)
+    unstaged_box.pack_start(scroll, True, True, 0)
+    self.pack_start(unstaged_box, False, False, 1)
+
+    unstaged_button_box = Gtk.Box()
+    add = Gtk.Button("Stage Files")
+    add.connect("clicked", self.stage_files)
+    unstaged_button_box.pack_start(add, False, False, 1)
+    ignore = Gtk.Button("Stage All")
+    ignore.connect("clicked", self.stage_files, True)
+    unstaged_button_box.pack_start(ignore, False, False, 1)
+    unstaged_box.pack_start(unstaged_button_box, False, False, 0)
+
+    # Section for Untracked Files
     untracked_box = Gtk.Box()
     untracked_box.set_orientation(Gtk.Orientation.VERTICAL)
     scroll = Gtk.ScrolledWindow()
@@ -132,11 +183,17 @@ class RepoBox(Gtk.Box):
     self.untracked_view.append_column(untracked_column_text)
     untracked_box.pack_start(scroll, True, True, 0)
 
-    add = Gtk.Button("Add Files")
-    add.connect("clicked", self.add_files)
-    untracked_box.pack_start(add, False, False, 1)
-    
-    self.pack_start(untracked_box, True, True, 0)
+
+    untracked_button_box = Gtk.Box()
+    add = Gtk.Button("Track Files")
+    add.connect("clicked", self.track_files)
+    untracked_button_box.pack_start(add, False, False, 1)
+    ignore = Gtk.Button("Ignore Files")
+    ignore.connect("clicked", self.ignore_files)
+    untracked_button_box.pack_start(ignore, False, False, 1)
+    untracked_box.pack_start(untracked_button_box, False, False, 0)
+    self.pack_start(untracked_box, False, False, 1)
+
     self.show_all()
 
   def update(self, **kwargs):
@@ -147,20 +204,97 @@ class RepoBox(Gtk.Box):
     """
     repo = kwargs.get('repo', None)
     if repo:
+      # Set Untracked Files
       untracked_list = Gtk.ListStore(bool, str)
       for untracked in repo.untracked_files:
         untracked_list.append((False, untracked))
       self.untracked_view.set_model(untracked_list)
-      #self.text.get_buffer().set_text(text or "No untracked files.")
-      self.last_repo = repo
 
-  def add_files(self, btn):
+      # Set Unstaged Files
+      unstaged_list = Gtk.ListStore(bool, str, str)
+      if repo.is_dirty():
+        self.refresh.set_sensitive(True)
+        diffs = repo.index.diff(None)
+        # Get stats for each diff in Added, Deleted, Modified, Renamed
+        for diff in diffs.iter_change_type('A'):
+          unstaged_list.append((False, 'A', diff.b_blob.name))
+        for diff in diffs.iter_change_type('D'):
+          unstaged_list.append((False, 'D', diff.a_blob.name))
+        for diff in diffs.iter_change_type('M'):
+          unstaged_list.append((False, 'M', diff.b_blob.name))
+        for diff in diffs.iter_change_type('R'):
+          unstaged_list.append((False, 'R', "{0} -> {1}".format(
+            diff.a_blob.name, diff.b_blob_name)))
+      else:
+        self.refresh.set_sensitive(False)
+      self.unstaged_view.set_model(unstaged_list)
+
+      self.current_repo = repo
+
+
+  def stage_files(self, btn, stage_all=False):
     """
     Add selected untracked files to be tracked with Git,
     then update the view.
 
     """
-    for row in self.untracked_view.get_model():
-      if row[0]:
-        print row[1]
-    self.update(repo=last_repo)
+    self.current_repo.index.add(
+      [row[2] for row in self.unstaged_view.get_model() if
+        stage_all or row[0]])
+    self.update(repo=self.current_repo)
+
+  def track_files(self, btn):
+    """
+    Add selected untracked files to be tracked with Git,
+    then update the view.
+
+    """
+    self.current_repo.index.add(
+      [row[1] for row in self.untracked_view.get_model() if row[0]])
+    self.update(repo=self.current_repo)
+
+  def ignore_files(self, btn):
+    """
+    Add selected files to the bottom of the .gitignore file. No special
+    heuristics are performed, each filename is inserted manually.
+
+    """
+    with open(os.path.join(self.current_repo.working_dir,
+        ".gitignore"), 'a+') as ignore:
+      ignore.seek(0, 2)
+      if ignore.read(1) != '\n':
+        ignore.write('\n')
+      for row in self.untracked_view.get_model():
+        if row[0]:
+          ignore.write(row[1] + '\n')
+    self.update(repo=self.current_repo)
+
+  def commit_files(self, btn):
+    """Commit all changes to the branch specified in HEAD."""
+    commit_msg = Gio.file_new_for_path(os.path.join(
+      self.current_repo.git_dir, "COMMIT_EDITMSG"))
+    commit_tab = self.window.create_tab_from_location(
+      commit_msg, None, 0, 0, True, True)
+    # Use a lock to ensure the file isn't closed and committed before the
+    # handler_id is created. Seems to be the only easy solution to sending
+    # the signal handler_id to the handler itself.
+    handler_id = []
+    self.commit_lock.acquire()
+    hid = self.window.connect("tab-removed", self.commit,
+      commit_tab, handler_id, self.current_repo)
+    handler_id.append(hid)
+    self.commit_lock.release()
+
+  def commit(self, window, tab, commit_tab, handler_id, repo):
+    if tab == commit_tab:
+      self.commit_lock.acquire()
+      doc = tab.get_document()
+      msg = doc.get_text(doc.get_start_iter(), doc.get_end_iter(), False)
+      msg = "\n".join([line.strip() for line in msg.split('\n') if
+        len(line.strip()) > 0 and not line.strip().startswith("#")])
+      print msg
+      print repo.index.commit(msg)
+      print repo.head.commit
+      if len(handler_id) > 0:
+        self.window.disconnect(handler_id[0])
+      self.commit_lock.release()
